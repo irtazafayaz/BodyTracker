aeafba431c0c272f4d3e5768f42565f3e81dc298
Irtaza Fiaz <ifiaz@i2cinc.com>
2022-11-22 21:12:32 +0500

    updating anchors wrt body movement

--- BodyTracker/ARViewContainer.swift
+++ BodyTracker/ARViewContainer.swift
@@ -10,12 +10,18 @@ import ARKit
 import RealityKit
 import SwiftUI
 
+private var bodySkeleton: BodySkeleton?
+private let bodySkeletonAnchor = AnchorEntity()
+
 struct ARViewContainer: UIViewRepresentable {
 
     typealias UIViewType = ARView
 
     func makeUIView(context: Context) -> ARView {
         let arrView = ARView(frame: .zero, cameraMode: .ar, automaticallyConfigureSession: true)
+        
+        arrView.setupForBodyTracking()
+        arrView.scene.addAnchor(bodySkeletonAnchor)
         return arrView
     }
     
@@ -23,6 +29,29 @@ struct ARViewContainer: UIViewRepresentable {
         
     }
     
+    
+}
+
+extension ARView: ARSessionDelegate {
+    
+    func setupForBodyTracking() {
+        let configuartion = ARBodyTrackingConfiguration()
+        self.session.run(configuartion)
+        self.session.delegate = self
+    }
+    
+    public func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+        for anchor in anchors {
+            if let bodyAnchor = anchor as? ARBodyAnchor {
+                if let skeleton = bodySkeleton {
+                    skeleton.update(with: bodyAnchor)
+                } else {
+                    bodySkeleton = BodySkeleton(for: bodyAnchor)
+                    bodySkeletonAnchor.addChild(bodySkeleton!)
+                }
+            }
+        }
+    }
     
 }
 
